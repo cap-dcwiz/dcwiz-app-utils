6ae01f271446c5936a3126557e6063893aba2f63
tefx <zhaomeng.zhu@gmail.com>
2023-02-16 02:48:40 +0800

    add get_router_maps

--- dcwiz_app_utils/app.py
+++ dcwiz_app_utils/app.py
@@ -1,3 +1,6 @@
+import pkgutil
+from importlib import import_module
+
 from fastapi import HTTPException, FastAPI
 from fastapi.responses import JSONResponse
 from dynaconf import Dynaconf
@@ -38,3 +41,21 @@ def set_config(_config: Dynaconf):
 
 def setup_app(app: FastAPI):
     app.add_exception_handler(HTTPException, http_exception_handler)
+
+
+def get_router_maps(_path, _name):
+    router_map = {}
+    for module in pkgutil.iter_modules(_path):
+        if not module.ispkg:
+            continue
+        try:
+            sub_app = import_module(f"{_name}.{module.name}.router")
+            router = getattr(sub_app, "router", None)
+            if router:
+                if module.name == "default":
+                    router_map["/"] = router
+                else:
+                    router_map[f"/{module.name}"] = router
+        except Exception as e:
+            print(e)
+    return router_map
